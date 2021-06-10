e03ad402262de9fcb81a1b27551192ac23d2621f
Pavel Svintsov <pahana707@gmail.com>
2021-06-09 21:21:28 -0700

    Initial

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from salary import calculate_salary
+from people import get_employees
+import datetime
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(datetime.date.today())
